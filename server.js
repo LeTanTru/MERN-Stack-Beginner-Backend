e055dbbb3a4ab18e3903eb09fc9ae1190b8e0fa5
LeTanTru <letantru@gmail.com>
2025-02-06 22:48:05 +0700

    update server.js

--- server.js
+++ server.js
@@ -25,13 +25,6 @@ const __dirname = path.resolve();
 
 app.use('/api/v1', productRoute);
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/frontend/dist')));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-	});
-}
-
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
